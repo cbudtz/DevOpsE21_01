9441e6e8775268f060d17424a462865bb7f846b0
Christian <chrbudtz@gmail.com>
2021-08-19 20:48:46 +0200

    Initial commit

---
+++ src/main/java/launch/Main.java
@@ -0,0 +1,27 @@
+package launch;
+
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.startup.Tomcat;
+
+import java.io.File;
+import java.util.Optional;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Tomcat tomcat = new Tomcat();
+        tomcat.setBaseDir("temp");
+        String port = Optional.ofNullable(System.getenv("PORT")).orElse("8080"); //Til Heroku
+        tomcat.setPort(Integer.parseInt(port));
+        tomcat.getConnector(); //Creates a default HTTP connector
+
+        tomcat.addWebapp("/", new File("src/main/webapp").getAbsolutePath());
+        try {
+            tomcat.start();
+            tomcat.getServer().await();
+        } catch (LifecycleException e) {
+            e.printStackTrace();
+        }
+    }
+}
